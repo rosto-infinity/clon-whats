7f59415239d5f2a9199f506e5208254c9e7eb35a
rostodev <rostodev@gmail.com>
2024-04-23 00:19:28 +0100

    partie01 frontend-clonewhatsapp-prof rostand lele

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+import ChatContainer from './compoments/ChatContainer'
+import Header from './compoments/Header'
+
+function App() {
+ 
+
+  return (
+  <div>
+    <main>
+    <Header />
+    <ChatContainer />
+      
+    </main>
+    
+  </div>
+  )
+}
+
+export default App
+
