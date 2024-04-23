a25bf499807fecea57baf41c2d781b96458ec155
rostodev <rostodev@gmail.com>
2024-04-23 20:46:00 +0100

    inscription

---
+++ src/compoments/auth/Auth.jsx
@@ -0,0 +1,10 @@
+import AuthForm from './AuthForm';
+const Auth = () => {
+  return (
+<div className=' absolute bg-white top-[8rem] bottom-0 left-[5rem] right-[5rem] lg:left-[35rem] lg:right-[35rem] z-30 shadow-lg '>
+      <AuthForm />
+    </div>
+  );
+} 
+
+export default Auth;
